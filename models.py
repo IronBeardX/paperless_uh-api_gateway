8e7f601a7afbc9a9610dadbead8452de1253f2c1
ironbeardx <alejandroyeroval@gmail.com>
2023-06-14 17:39:26 -0400

    some models implementations

---
+++ models.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str = None
+
+class User(BaseModel):
+    username: str
+    email: str
+    full_name: str = None
+    disabled: bool = None
+
+class UserInDB(User):
+    hashed_password: str
